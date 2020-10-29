98c68609d5537963bf756c8c61a7ddb16321597b
Giannadimi <giannastakos@gmail.com>
2020-10-29 15:30:46 +0200

    Updates

---
+++ precache-manifest.d86668f09177978caf0c7f42eb3b8975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae21e1aded63c0cdc3fbb482a853462",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c57bf30d375ecccffba8",
+    "url": "/monsters-rolodex/static/css/main.87520e3b.chunk.css"
+  },
+  {
+    "revision": "6788f37076e290597b34",
+    "url": "/monsters-rolodex/static/js/2.50bf30cc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.50bf30cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c57bf30d375ecccffba8",
+    "url": "/monsters-rolodex/static/js/main.f02553ef.chunk.js"
+  },
+  {
+    "revision": "8ddc55c1ed688e04beb6",
+    "url": "/monsters-rolodex/static/js/runtime-main.c30c29c6.js"
+  }
+]);
