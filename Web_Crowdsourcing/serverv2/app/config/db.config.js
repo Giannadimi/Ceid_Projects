00412304dd622754226df363434875d324642974
Giannoula Dimitrouka <50720784+Giannadimi@users.noreply.github.com>
2022-08-29 19:17:17 +0300

    Add files via upload

---
+++ Web_Crowdsourcing/serverv2/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "V@g1@2641045152",
+  DB: "webproject_2020",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 120000000,
+    idle: 10000,
+  },
+};
